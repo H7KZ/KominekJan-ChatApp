8b4c75550e5dc48319031a4546bac4ef60c684d5
H7KZ <74021016+H7KZ@users.noreply.github.com>
2022-04-03 19:33:04 +0200

    hotfix link

--- src/routes/verify/success.svelte
+++ src/routes/verify/success.svelte
@@ -2,7 +2,7 @@
 	<div class="w-full font-ms font-semibold text-2xl text-grayWhite text-center">
 		<h1 class="font-bold text-[#cbff6a]">
 			You were successfully verified!<br />
-			<a href="/login" class="text-grayWhite font-semibold italic">click here to log in</a>
+			<a href="/account/login" class="text-grayWhite font-semibold italic">click here to log in</a>
 		</h1>
 	</div>
 </div>
